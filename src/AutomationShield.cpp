2c62c445e6425dd5f6f91a3deff06bf7152fb814
TiborKonkoly <37699408+TiborKonkoly@users.noreply.github.com>
2018-03-26 21:04:04 +0200

    Last change by Tibor on 26.03.2018 at 21:03

--- src/AutomationShield.cpp
+++ src/AutomationShield.cpp
@@ -4,7 +4,7 @@
   Authors: Tibor Konkoly, Gabor Penzinger, [...], and Gergely Takacs
   2017-2018.
   Released into the public domain.
-  Last change by Tibor Konkoly on 25.03.2018 at 21:05.
+  Last change by Tibor Konkoly on 26.03.2018 at 20:51.
 */
 
 
@@ -13,17 +13,14 @@
 
         
 Optical::Optical(){
-  
+ 
 }
 
-void Optical::begin(int pin1 , int pin2, int pin3){
-    _pin1 = pin1;
-    _pin2 = pin2;
-    _pin3 = pin3; 
-  
-  pinMode(_pin1, INPUT);
-  pinMode(_pin2, OUTPUT);
-  pinMode(_pin3, INPUT); 
+void Optical::begin(void){                  // begin function initializes the pins used by the hardware. Optoshield uses three pins, pin A1 is used by the LDR, 
+                                            //pin A0 is connected to the runner of the potentiometer and digital pin 3 is for setting the intensity of the leds' light                                            
+  pinMode(OPTICAL_YPIN, INPUT);
+  pinMode(OPTICAL_UPIN, OUTPUT);
+  pinMode(OPTICAL_RPIN, INPUT); 
 }
 
 
@@ -31,19 +28,19 @@ void Optical::actuatorWrite(int value){
  
   if(value <= 255){                                                 // nested if statement, if the condition is true check the following
       if(value > 0){                                                  // if the second condition is also true, write the value of the sensor
-        analogWrite(_pin2,value);
+        analogWrite(OPTICAL_UPIN,value);
       }
     }
     else {Serial.println("The number You added doesn't fit the conditions, please choose a number between 0-255");} // if any of the statements is true, you receive a report 
 }
 
 int Optical::sensorRead(){
-  int _valueRead = analogRead(_pin1);
+  int _valueRead = analogRead(OPTICAL_YPIN);
   return _valueRead;
 }
 
 int Optical::referenceRead(){
-  int _valueRead = analogRead(_pin3);
+  int _valueRead = analogRead(OPTICAL_RPIN);
   return _valueRead;
 }
             
