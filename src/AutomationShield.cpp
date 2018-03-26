980e8728ca3fb9c1f742febed6657b08cb95d1fa
TiborKonkoly <37699408+TiborKonkoly@users.noreply.github.com>
2018-03-26 16:51:59 +0200

    Tibor 26.03.2018
    First  attempt to make a library.

--- src/AutomationShield.cpp
+++ src/AutomationShield.cpp
@@ -1,44 +1,52 @@
-/*
-  AutomationShield.cpp
+/* 
+ AutomationShield.cpp
   Arduino library for teaching control engineering.
   Authors: Tibor Konkoly, Gabor Penzinger, [...], and Gergely Takacs
   2017-2018.
   Released into the public domain.
+  Last change by Tibor Konkoly on 25.03.2018 at 21:05.
 */
+
 
 #include "AutomationShield.h"
+#include "Arduino.h"
 
-/*
----Optical Shield---
-*/
-
-void OpticalClass::begin(void){
-// Example begin function
-  pinMode(OPTICAL_UPIN,OUTPUT); // Set output pin
+        
+Optical::Optical(){
+  
 }
 
-void OpticalClass::actuatorWrite(float u){
-  u = constrain(u,0.0,100.0); //Constrains to 0-100%
-  int uPWM = map((int)u,0,100,0,255);
-  analogWrite(OPTICAL_UPIN,uPWM);
+void Optical::begin(int pin1 , int pin2, int pin3){
+    _pin1 = pin1;
+    _pin2 = pin2;
+    _pin3 = pin3; 
+  
+  pinMode(_pin1, INPUT);
+  pinMode(_pin2, OUTPUT);
+  pinMode(_pin3, INPUT); 
 }
 
-float OpticalClass::sensorRead(void){
-  //
-  // return y;
+
+void Optical::actuatorWrite(int value){
+ 
+  if(value <= 255){                                                 // nested if statement, if the condition is true check the following
+      if(value > 0){                                                  // if the second condition is also true, write the value of the sensor
+        analogWrite(_pin2,value);
+      }
+    }
+    else {Serial.println("The number You added doesn't fit the conditions, please choose a number between 0-255");} // if any of the statements is true, you receive a report 
 }
 
-float OpticalClass::referenceRead(void){
-  //
-  // return r;
+int Optical::sensorRead(){
+  int _valueRead = analogRead(_pin1);
+  return _valueRead;
 }
 
-OpticalClass Optical;
+int Optical::referenceRead(){
+  int _valueRead = analogRead(_pin3);
+  return _valueRead;
+}
+            
 
-/*
----Motor Shield---
-*/
-
-
-// Motor shield....:
+                     
 
