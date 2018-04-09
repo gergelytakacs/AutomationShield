30892c9829fbfef017de57d1779027bdd911abf6
TiborKonkoly <37699408+TiborKonkoly@users.noreply.github.com>
2018-04-09 21:25:32 +0200

    The latest version 09.04.2018

--- src/AutomationShield.cpp
+++ src/AutomationShield.cpp
@@ -4,25 +4,19 @@
   Authors: Tibor Konkoly, Gabor Penzinger, [...], and Gergely Takacs
   2017-2018.
   Released into the public domain.
-  Last change by Tibor Konkoly on 07.04.2018 at 18:48 .
+  Last change by Tibor Konkoly on 09.04.2018 at 21:15 .
 */
 
 
 #include "AutomationShield.h"
 #include "Arduino.h"
 
-float mapFloat(float x, float in_min, float in_max, float out_min, float out_max)
+float AutomationShieldClass::mapFloat(float x, float in_min, float in_max, float out_min, float out_max) // same as Arudino map() but with floating point numbers
 {
   return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min; // linear mapping, same as Arduino map()
 }
 
-// AutomationShield
-
-AutomationShield::AutomationShield(){
-  
-}
-
-void AutomationShield::error(char *str) // Error handler function
+void AutomationShieldClass::error(char *str) // Error handler function
 {
   #if ECHO_TO_SERIAL                    // If Serial Echo is turned on in the DEFINE (by advanced user)
   Serial.print("Error: ");
@@ -32,13 +26,13 @@ void AutomationShield::error(char *str) // Error handler function
   while (1);                              // Stop all activity in busy cycle, so user definitely knows what's wrong.
 }
 
+
+
+AutomationShieldClass AutomationShield; // Construct instance (define)
+
 // Opto
 
-Opto::Opto(){
-  
-}
-
-void Opto::calibration(){                           // a function determines the maximal and minimal value of the LDR, which is crutial for accurate measurements
+void OptoClass::calibration(){                           // a function determines the maximal and minimal value of the LDR, which is crutial for accurate measurements
   Serial.println("Calibration is running...");
   delay(3000);                                      // we start with a 3s delay, which is needed for the value of LDR to get steady
   
@@ -64,7 +58,7 @@ void Opto::calibration(){                           // a function determines the
 
 
 
-void Opto::begin(void){                  // begin function initializes the pins used by the hardware. Optoshield uses three pins, pin A1 is used by the LDR,                                          //pin A0 is connected to the runner of the potentiometer and digital pin 3 is for setting the intensity of the leds' light                                            
+void OptoClass::begin(void){                  // begin function initializes the pins used by the hardware. Optoshield uses three pins, pin A1 is used by the LDR,                                          //pin A0 is connected to the runner of the potentiometer and digital pin 3 is for setting the intensity of the leds' light                                            
   pinMode(OPTO_YPIN, INPUT);             // analog pin A2 is connected to the auxiliary LDR
   pinMode(OPTO_UPIN, OUTPUT);
   pinMode(OPTO_RPIN, INPUT); 
@@ -72,26 +66,26 @@ void Opto::begin(void){                  // begin function initializes the pins 
 }
 
 
-void Opto::actuatorWrite(float value){          // actuatorWrite() sets the LEDs' brightness, using a floating point number from 0 (min) to 100 (max)
-  _convertedValue = mapFloat(value,0.00,100.00,0.00,255.00);
+void OptoClass::actuatorWrite(float value){          // actuatorWrite() sets the LEDs' brightness, using a floating point number from 0 (min) to 100 (max)
+  _convertedValue = AutomationShield.mapFloat(value,0.00,100.00,0.00,255.00);
   analogWrite(OPTO_UPIN,_convertedValue); 
 }
 
 
-float Opto::sensorRead(){
+float OptoClass::sensorRead(){
    _sensorRead = analogRead(OPTO_YPIN);
    
    if(indicator){                                                   // with an if statement I can check, if the calibration function was called (recommended)
-   _sensorValue = mapFloat(_sensorRead, _minVal, _maxVal, 0.00, 100.00); 
+   _sensorValue = AutomationShield.mapFloat(_sensorRead, _minVal, _maxVal, 0.00, 100.00); 
    }
    else{
-   _sensorValue = mapFloat(_sensorRead, 515.00, 1000.00, 0.00, 100.00); // when the calibration was not called we use static values, which were the most common values during the tests
+   _sensorValue = AutomationShield.mapFloat(_sensorRead, 515.00, 1000.00, 0.00, 100.00); // when the calibration was not called we use static values, which were the most common values during the tests
    }
   return _sensorValue;
 }
 
 
-float Opto::sensorReadVoltage(){   // sensorReadVoltage returns the Voltage on the LDR in the tube
+float OptoClass::sensorReadVoltage(){   // sensorReadVoltage returns the Voltage on the LDR in the tube
   float k = (5.00 / 1023.00);     // constant that allows us to convert the analog values from the sensor into voltage 
   _valueRead = analogRead(OPTO_YPIN);
   _sensorVoltage = _valueRead * k;
@@ -99,7 +93,7 @@ float Opto::sensorReadVoltage(){   // sensorReadVoltage returns the Voltage on t
 }
 
 
-float Opto::sensorAuxRead(){      // sensorAuxRead returns the Voltage on the auxiliary LDR as a floating point number
+float OptoClass::sensorAuxRead(){      // sensorAuxRead returns the Voltage on the auxiliary LDR as a floating point number
  float k = (5.00 / 1023.00);      // constant that allows us to convert the analog values from the sensor into voltage 
   _auxRead = analogRead(OPTO_YAUX);
   _auxVoltage = _auxRead * k;
@@ -108,10 +102,10 @@ float Opto::sensorAuxRead(){      // sensorAuxRead returns the Voltage on the au
 
 
 
-float Opto::referenceRead(){           // referenceRead function returns the reference value of the potentiometer, which was set by the user, in percents
+float OptoClass::referenceRead(){           // referenceRead function returns the reference value of the potentiometer, which was set by the user, in percents
    _referenceRead = analogRead(OPTO_RPIN);
-   _referenceValue = mapFloat(_referenceRead,0.00,1023.00,0.00,100.00);
+   _referenceValue = AutomationShield.mapFloat(_referenceRead,0.00,1023.00,0.00,100.00);
   return _referenceValue;
 }
 
-
+OptoClass OptoShield; // Construct instance (define) 
