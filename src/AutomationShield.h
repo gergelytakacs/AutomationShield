3e1d53ae49b66fa6940704fee8adebd5c3f1317c
kulho <jakubkulhanek82103@email.com>
2018-04-09 12:30:27 +0200

    My take on PID
    So general idea goes like this. Function initialzePID should be called once in setup to specify user parameters. Then in loop function PID will be called with either 1 or 2 parameters. User can either call it with requested value and current value or with his own error. This comes from the idea of block representation of PID control, where in flows the error e and out goes output u (as it is for didactic purposes). More explanation can be found in the code. Unfortunately, I have no idea how will the code behave as I have nothing to test it on.

--- src/AutomationShield.h
+++ src/AutomationShield.h
@@ -31,8 +31,6 @@
  class AutomationShield{
 
   public:
-    #define REV 1 //reverse acting
-    #define DIR 0
   // Constructor
   AutomationShield();
   
@@ -40,26 +38,26 @@
  float mapFloat(float x, float in_min, float in_max, float out_min, float out_max);
 //constrainFloat?
  void error(char *str);
-  float constrain(float x, float min_x, float max_x); 
-  float pid(float err,float input,float Kp,float Ki,float Kd,float outMin,float outMax,int direct);
-  float pid1(float err,float Kp,float Ti,float Td,float outMin, float outMax); 
+  float constrain(float x, float min_x, float max_x);
+  void initializePID(float _KP, float _KI, float _KD, int _Ts, float _min_u, float _max_u);
+  float PID(float request_val, float current_val);
+  float PID(float user_error);
+     
   private:
-    float integral;
-    float derivative;
-    float lastinput;
-    float error[3] = {0,0,0};
-    float out[2] = {0,0};
-    float r_p;
-    float r_i;
-    float r_d;
-    float q0;
-    float q1;
-    float q2;
-    float output;
+     float Ts;
+     float KP;
+     float KI;
+     float KD;
+     float integral=0;
+     float derivative=0;
+     float error_pid=0;
+     float error_prior=0;
+     float u=0;
+     unsigned long now;
+     unsigned long time_prior=0;
+     int time_change;
+     int real_Ts;
  }; // end of the class
-
-
-
 
  class Opto{
 
