1cbbc92d94873902e1d406e54fcde54c78966939
Richard Koplinger <r.koplinger@gmail.com>
2018-04-16 21:45:19 +0200

    fixed the bug in compare register

--- src/Timer.cpp
+++ src/Timer.cpp
@@ -25,23 +25,23 @@ bool Timer::setSamplingPeriod(unsigned long microseconds){
         
         if (cycles < timer1Resolution){
           TCCR1B |= (0 << CS12) | (0 << CS11) | (1 << CS10); // no prescaling
-          OCR1A = cycles;            // compare match register
+          OCR1A = cycles-1;            // compare match register
         }
         else if(cycles < timer1Resolution * 8){
           TCCR1B |= (0 << CS12) | (1 << CS11) | (0 << CS10); // 8 prescaler 
-          OCR1A = cycles/8;            // compare match register
+          OCR1A = (cycles/8)-1;            // compare match register
         }
         else if(cycles < timer1Resolution * 64){
           TCCR1B |= (0 << CS12) | (1 << CS11) | (1 << CS10); // 64 prescaler 
-          OCR1A = cycles/64;            // compare match register
+          OCR1A = (cycles/64)-1;            // compare match register
         }
         else if(cycles < timer1Resolution * 256){
           TCCR1B |= (1 << CS12) | (0 << CS11) | (0 << CS10); // 256 prescaler 
-          OCR1A = cycles/256;            // compare match register
+          OCR1A = (cycles/256)-1;            // compare match register
         }
         else if(cycles < timer1Resolution * 1024){
           TCCR1B |= (1 << CS12) | (0 << CS11) | (1 << CS10); // 1024 prescaler 
-          OCR1A = cycles/1024;            // compare match register
+          OCR1A = (cycles/1024)-1;            // compare match register
         }
         else{
           return false;
