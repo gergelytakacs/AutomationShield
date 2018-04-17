44ab16529267ace159366370845944f6e742a0bb
Gergely Takács <37662064+gergelytakacs@users.noreply.github.com>
2018-04-17 14:46:53 +0200

    Changed error variable name, changed colliding constrain
    Changed error variable name, changed colliding constrain

--- src/AutomationShield.h
+++ src/AutomationShield.h
@@ -33,14 +33,14 @@
   // Methods
  float mapFloat(float x, float in_min, float in_max, float out_min, float out_max);
  void error(char *str);
-float constrain(float x, float min_x, float max_x); 
+float constrainFloat(float x, float min_x, float max_x); 
   float pid(float err,float input,float Kp,float Ki,float Kd,float outMin,float outMax,int direct);
   float pid1(float err,float Kp,float Ti,float Td,float outMin, float outMax); 
   private:
     float integral;
     float derivative;
     float lastinput;
-    float error[3] = {0,0,0};
+    float e[3] = {0,0,0};
     float out[2] = {0,0};
     float r_p;
     float r_i;
