565f28de450ebe361ac92af8b51430a42529dad6
Gergely Takács <gergelytakacs@gergelytakacs.com>
2018-05-03 09:15:29 +0200

    Just an empty header so far

---
+++ src/_Model.cpp
@@ -0,0 +1,4 @@
+//#include "Model.h"
+
+
+
---
+++ src/_Model.h
@@ -0,0 +1,16 @@
+#ifndef MODEL_H_
+#define MODEL_H_
+
+class ModelClass{
+  public:
+  void  loremipsum();
+  
+  private:
+//  float _convertedValue;
+
+}; // end of the Model class
+
+
+//extern ModelClass Model; // Declare external instance
+
+#endif
