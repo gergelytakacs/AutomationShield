461d2c8d2d94078b8f490c6611ed0d0791a15b51
Richard Koplinger <r.koplinger@gmail.com>
2018-05-09 13:12:57 +0200

    edited spaces

--- examples/HeatShield_PID/HeatShield_PID.ino
+++ examples/HeatShield_PID/HeatShield_PID.ino
@@ -9,10 +9,10 @@ bool enable = false;
 
 void setup(void) {
 
-	Serial.begin(9600);          
+  Serial.begin(9600);          
   HeatShield.begin(); 
-	Sampling.interruptInitialize(1000000); //period in us
-	Sampling.setInterruptCallback(stepEnable); 
+  Sampling.interruptInitialize(1000000); //period in us
+  Sampling.setInterruptCallback(stepEnable); 
   PIDAbs.setKp(0.5);
   PIDAbs.setTi(0.5);
   PIDAbs.setTd(0.5);
@@ -20,31 +20,31 @@ void setup(void) {
 
 void loop(void) {
 
-	if (enable) {
+  if (enable) {
     
     step();
     enable=false;
-	}
+  }
 }
 
 void stepEnable() {
 
-	enable=true;
+  enable=true;
 }
 
 void step(){
   
-    HeatShield.actuatorWrite(u);
-    r=40.0;
-    y=HeatShield.sensorReadTemperature();
-    e=r-y;
-    u=PIDAbs.compute(e,0.0,100.0,-100.0,100.0);
+  HeatShield.actuatorWrite(u);
+  r=40.0;
+  y=HeatShield.sensorReadTemperature();
+  e=r-y;
+  u=PIDAbs.compute(e,0.0,100.0,-100.0,100.0);
     
-    Serial.print(y);
-    Serial.print(" ");
-    Serial.print(u);
-    Serial.print(" ");
-    Serial.println(r);
+  Serial.print(y);
+  Serial.print(" ");
+  Serial.print(u);
+  Serial.print(" ");
+  Serial.println(r);
 }
 
 
