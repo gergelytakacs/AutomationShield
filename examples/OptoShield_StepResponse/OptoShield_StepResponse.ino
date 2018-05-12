fe4faac7a3c0666c236ef3aa3aec3f1c78b4fdb2
TiborKonkoly <37699408+TiborKonkoly@users.noreply.github.com>
2018-05-12 16:28:42 +0200

    OptoShield _StepResponse

---
+++ examples/OptoShield_StepResponse/OptoShield_StepResponse.ino
@@ -0,0 +1,22 @@
+#include "AutomationShield.h"
+
+float Setpoint = 50.00;
+
+void setup() {
+  
+Serial.begin(9600);
+OptoShield.begin();
+OptoShield.calibration();
+delay(1000);
+OptoShield.actuatorWrite(Setpoint);
+}
+
+void loop() {
+
+  float senzor = OptoShield.sensorRead();
+
+  Serial.print(Setpoint);
+  Serial.print(" ");
+  Serial.println(senzor);
+
+}
