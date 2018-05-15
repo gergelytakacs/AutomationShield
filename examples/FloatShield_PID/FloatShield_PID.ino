ea768c6c655bfcea4d80048817352c7f50ae46fc
Gabor7697 <37667063+Gabor7697@users.noreply.github.com>
2018-05-15 10:55:17 +0200

    Update FloatShield_PID.ino

--- examples/FloatShield_PID/FloatShield_PID.ino
+++ examples/FloatShield_PID/FloatShield_PID.ino
@@ -3,22 +3,24 @@
 #include <FloatShield.h>
 
 int dist;
-unsigned long int Ts = 100;
-bool enable = false;
-int y;
-float u;
-int r;
-float Ti,Td,Kp;
-float error;
+unsigned long int Ts = 100; //sampling time in ms
+bool enable = false; //flag 
+int y;  //output
+float u; //input
+int r; //reference
+float Ti,Td,Kp; // PID constants
+float error; //error
 void setup() {
-  Serial.begin(115200);
-  FloatShield.initialize();
-  FloatShield.calibrate();
-  Sampling.interruptInitialize(Ts*1000);
-  Sampling.setInterruptCallback(StepEnable);
+  Serial.begin(115200); //start serial communication
+  FloatShield.initialize(); //FloatShield initialization
+  FloatShield.calibrate(); //FloatShield calibration to more accurate measurements
+  Sampling.interruptInitialize(Ts*1000); //Sampling initialization in microseconds
+  Sampling.setInterruptCallback(StepEnable); // seting the interrupt functon
+  //Setting the PID constants
   PIDAbs.setKp(0.86);
   PIDAbs.setKi(1.729);
   PIDAbs.setKd(0.1081);
+  //Getting constants needed for calculating PID in absolute form
   Ti = PIDAbs.getTi();
   Td = PIDAbs.getTd();
   Kp = PIDAbs.getKp();
@@ -29,22 +31,22 @@ void loop()
   if(enable)
   {
     Step();
-    enable = false;
+    enable = false; //enable flag becomes false after the execution of the Step() function
   }
 }
 
-void StepEnable(void)
+void StepEnable(void) // interrupt function
 {
-  enable = true;
+  enable = true; // when the interrupt function is called, the enable is true
 }
 
 void Step (void)
 {
-  y = FloatShield.positionPercent();
-  r = FloatShield.referencePercent();
+  y = FloatShield.positionPercent(); //reading the  sensor value
+  r = FloatShield.referencePercent(); //reading the reference value
 
-  error = r - y;
-  u = PIDAbs.compute(error,25,55,0,100);
+  error = r - y; 
+  u = PIDAbs.compute(error,25,55,0,100); // absolute PID function with anti-wind up and saturation
   FloatShield.ventInPercent(u);
   Serial.print(r);
   Serial.print(", ");
