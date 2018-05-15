c66e4b59dd5356155a766bf1504420d4842c011b
TiborKonkoly <37699408+TiborKonkoly@users.noreply.github.com>
2018-05-15 12:19:42 +0200

    Add files via upload

---
+++ examples/MotoShield_StepResponse/MotoShield_StepResponse.ino
@@ -0,0 +1,35 @@
+#include "AutomationShield.h"
+
+float Setpoint = 30.00;
+
+
+float senzor = 0.00; // variables
+float converted = 0.00;
+
+float maximum = 24.56; // determining the boundaries of the mapFloat() function
+float minimum = 10.27;
+
+void setup() {
+  
+Serial.begin(9600);
+MotoShield.begin(); // initializing pins
+
+MotoShield.setDirection(true);       // starting the motor
+MotoShield.setMotorSpeed(Setpoint);
+}
+
+void loop() {
+
+ 
+senzor = MotoShield.readRevolutions();  // measuring the RPM
+converted = AutomationShield.mapFloat(senzor,minimum,maximum,0.00,100.00); // converting the RPM
+   
+  
+
+   Serial.print(Setpoint);
+  Serial.print(" ");
+  Serial.println(converted); 
+
+}
+
+
