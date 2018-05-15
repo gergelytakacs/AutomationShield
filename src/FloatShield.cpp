329b003c8dad8ef6f0bd9144bdcc6038d3c9396e
Gabor7697 <37667063+Gabor7697@users.noreply.github.com>
2018-05-15 10:34:20 +0200

    Added comments

--- src/FloatShield.cpp
+++ src/FloatShield.cpp
@@ -24,13 +24,13 @@ int FloatShieldClass::positionPercent() {
     lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
     
     if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-        value =measure.RangeMilliMeter;
+        value =measure.RangeMilliMeter; // sensor reading in mm
     } else {}
     
     if (value < minimum) {value = minimum;}
     else if (value > maximum) {value = lastValue;}
     
-    pos = map(value,minimum,maximum,100,0);
+    pos = map(value,minimum,maximum,100,0); // converts sensor readings in mm to percents
     
     lastValue = value;
     return pos;
@@ -41,7 +41,7 @@ int FloatShieldClass::positionMillimeter() {
     lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
     
     if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-        value =measure.RangeMilliMeter;
+        value =measure.RangeMilliMeter; // sensor reading in mm
     } else {}
     
     if (value < minimum) {value = minimum;}
@@ -50,7 +50,7 @@ int FloatShieldClass::positionMillimeter() {
     
     pos = value;
     
-    return pos;
+    return pos; // returns position in millimeters
 }
 
 
@@ -69,31 +69,33 @@ float FloatShieldClass::manualControl() {
 
 void FloatShieldClass::calibrate(void){
  int i=0;
- int temp;
-  minimum = 5000;
+ int temp; // temporary variable
+  minimum = 5000;  // we initially set the minimum to a very big number to make sure that 
+                   //the measured minimum will be smaller that this value
  analogWrite(vent,255);
  delay(1000);
- for(i=0;i <= 100;i++)
+ for(i=0;i <= 100;i++) //measure the minimum 100 times
  {
    VL53L0X_RangingMeasurementData_t measure;
     lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
     
     if (measure.RangeStatus != 4) {  // phase failures have incorrect data
-        temp =measure.RangeMilliMeter;
+        temp =measure.RangeMilliMeter; // variable temp stores the measured value
     } else {}
 
-   if(temp < minimum)
+   if(temp < minimum) 
    {
-    minimum = temp;
+    minimum = temp; //if the temporary variable is smaller than the actual minimum, 
+                    //the temp. variable will be the actual minimum value.
    }
 
-   delay(10);
+   delay(10); // wait 10 ms between measurements
  }
  analogWrite(vent,0);
  delay(1000);  
  int tempm =0;
-  maximum = 0;
- for(i=0;i <=100;i++)
+  maximum = 0; // we initially set maximum to 0, so the measued maximum will be greater than this number
+ for(i=0;i <=100;i++) // measure the maximum 100 times
  {
   VL53L0X_RangingMeasurementData_t measure;
     lox.rangingTest(&measure, false); // pass in 'true' to get debug data printout!
@@ -104,10 +106,11 @@ void FloatShieldClass::calibrate(void){
   
   if(tempm > maximum)
   {
-    maximum = tempm;
+    maximum = tempm; //if the temporary variable is greater than the actual maximum, 
+                    //the tempm. variable will be the actual maximum value.
   }
   
-  delay(10);
+  delay(10); // wait 10 ms between measurements
  }
 }
 FloatShieldClass FloatShield;
