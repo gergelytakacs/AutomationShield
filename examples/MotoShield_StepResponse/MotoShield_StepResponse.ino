66147b01698e728f7ec1d06f4fb94d6233f90f25
TiborKonkoly <37699408+TiborKonkoly@users.noreply.github.com>
2018-05-24 20:34:51 +0200

    Update MotoShield_StepResponse.ino

--- examples/MotoShield_StepResponse/MotoShield_StepResponse.ino
+++ examples/MotoShield_StepResponse/MotoShield_StepResponse.ino
@@ -1,32 +1,56 @@
 #include "AutomationShield.h"
-
-float Setpoint = 30.00;
-
-
-float senzor = 0.00; // variables
-float converted = 0.00;
-
-float maximum = 24.56; // determining the boundaries of the mapFloat() function
-float minimum = 10.27;
-
-void setup() {
-  
-Serial.begin(9600);
-MotoShield.begin(); // initializing pins
-
-MotoShield.setDirection(true);       // starting the motor
-MotoShield.setMotorSpeed(Setpoint);
-}
-
-void loop() {
-
-senzor = MotoShield.readRevolutions(50);  // measuring the RPM
-converted = AutomationShield.mapFloat(senzor,minimum,maximum,0.00,100.00); // converting the RPM
    
-  Serial.print(Setpoint);
-  Serial.print(" ");
-  Serial.println(converted); 
-
-}
-
-
+   float Setpoint = 100.00;        // reference
+   
+   bool enable=false;              // variables for sampling
+   unsigned long int curTime=0;
+   unsigned long int prevTime=0;
+   
+   unsigned long Ts = 0.1;         // sampling time
+   
+   
+   float senzor = 0.00;            // variables
+   float converted = 0.00;
+   
+                                   // boundaries of converting
+   float maximum = 24.36;          
+   float minimum = 10.27;
+   
+   void setup() {
+   
+   Serial.begin(9600);
+   
+   Sampling.interruptInitialize(Ts * 1000);
+   Sampling.setInterruptCallback(stepEnable);
+                                   // sampling
+   
+   MotoShield.begin();             // board initialization   
+   MotoShield.setDirection(true);  // starting the motor
+   MotoShield.setMotorSpeed(Setpoint);
+   }
+   
+   void loop() {
+   
+   curTime=millis();
+   if (enable) {
+   step();
+   enable=false;
+   
+   } // end of the if statement  
+   } // end of the loop
+   
+   void stepEnable(){
+   enable=true;
+   }
+   
+   void step(){
+   senzor = MotoShield.readRevolutions(50);  
+                                   // measuring rpm
+   
+   converted = AutomationShield.mapFloat(senzor,minimum,maximum,0.00,100.00); 
+                                   // converting rpm into %
+   
+   Serial.print(Setpoint);
+   Serial.print(",");
+   Serial.println(converted); 
+   }
