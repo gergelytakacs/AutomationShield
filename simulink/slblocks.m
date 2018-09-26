8b9f15d3712b1ea5054634e7eac07f8673051961
gergelytakacs <gergelytakacs@gergelytakacs.com>
2018-09-26 16:50:07 +0200

    Simulink API initial commit
    Initial commit

---
+++ simulink/slblocks.m
@@ -0,0 +1,13 @@
+function blkStruct = slblocks
+		% This function specifies that the library should appear
+		% in the Library Browser
+		% and be cached in the browser repository
+
+		Browser.Library = 'AutomationShield';
+		% 'mylib' is the name of the library
+
+		Browser.Name = 'AutomationShield';
+		% 'My Library' is the library name that appears 
+             % in the Library Browser
+
+		blkStruct.Browser = Browser; 
