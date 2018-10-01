a599ebc4e5b9367dd8bcf9d961b21b30d08b9207
gergelytakacs <gergelytakacs@gergelytakacs.com>
2018-10-01 11:08:36 +0200

    OptoShield SysID Examples
    Added OptoShield system identification examples

---
+++ matlab/OptoShield/identifyOptoShield_Step.m
@@ -0,0 +1,34 @@
+%   OptoShield system identification experiment
+% 
+%   Aquires data from the OptoShield then fits 1st order model to
+%   identification results.
+%
+%   This code is part of the AutomationShield hardware and software
+%   ecosystem. Visit http://www.automationshield.com for more
+%   details. This code is licensed under a Creative Commons
+%   Attribution-NonCommercial 4.0 International License.
+% 
+%   Created by Gergely Takács. 
+%   Last update: 01.10.2018.
+
+plotOptoShield_Step
+load resultID
+Ts=0.005
+Y=resultID(:,1);
+U=resultID(:,2);
+
+data = iddata(Y,U,Ts);      
+dataselection = datafull([1:400]);   
+Opt = procestOptions;                 
+model = procest(dataselection,'P1', Opt);
+
+compare(dataselection,model);
+pause
+
+figure(1)
+[Ym, FIT, X0]=compare(datafull,model);
+Ym=Ym.y(:,1);
+plot([Y U Ym])
+legend('Output','Input','Model')
+xlabel('Samples ()')
+ylabel('Input, Output (%)')
---
+++ matlab/OptoShield/plotOptoShield_Step.m
@@ -0,0 +1,23 @@
+%   OptoShield system identification experiment
+% 
+%   Aquires data for system identification and plots
+%   the data.
+%
+%   This code is part of the AutomationShield hardware and software
+%   ecosystem. Visit http://www.automationshield.com for more
+%   details. This code is licensed under a Creative Commons
+%   Attribution-NonCommercial 4.0 International License.
+% 
+%   Created by Gergely Takács. 
+%   Last update: 01.10.2018.
+
+resultID=readExperiment(7*200,'COM22',9600);
+save resultID resultID
+
+plot(resultID)
+hold on
+legend('Output','Input')
+xlabel('Sample no. (-)')
+ylabel('Input, output level (%)')
+grid on
+
