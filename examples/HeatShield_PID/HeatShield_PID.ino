330a17a024236c599b02d5a60823db950bf622e0
gergelytakacs <gergelytakacs@gergelytakacs.com>
2018-10-19 19:30:39 +0200

    Minor fixes

--- examples/HeatShield_PID/HeatShield_PID.ino
+++ examples/HeatShield_PID/HeatShield_PID.ino
@@ -1,50 +1,90 @@
-#include "AutomationShield.h"
+/*
+  HeatShield closed-loop PID response example
 
-float r;
-float y;
-float u=0.0;
-float e;
-
-bool enable = false;
-
-void setup(void) {
-
-  Serial.begin(9600);          
-  HeatShield.begin(); 
-  Sampling.interruptInitialize(1000000); //period in us
-  Sampling.setInterruptCallback(stepEnable); 
-  PIDAbs.setKp(10.3022952672455);
-  PIDAbs.setTi(300);
-  PIDAbs.setTd(0);
-}
-
-void loop(void) {
-
-  if (enable) {
-    
-    step();
-    enable=false;
-  }
-}
-
-void stepEnable() {
-
-  enable=true;
-}
-
-void step(){
+  PID feedback control of the heating block temperature.
   
-  HeatShield.actuatorWrite(u);
-  r=40.0;
-  y=HeatShield.sensorReadTemperature();
-  e=r-y;
-  u=PIDAbs.compute(e,0.0,100.0,-100.0,100.0);
-    
-  Serial.print(y);
-  Serial.print(" ");
-  Serial.print(u);
-  Serial.print(" ");
-  Serial.println(r);
+  This example initializes the sampling and PID control 
+  subsystems from the AutomationShield library and starts a 
+  predetermined reference trajectory for the heating block
+  temperature. 
+  
+  Upload the code to your board, then open the Serial
+  Plotter function in your Arduino IDE. You may change the
+  reference trajectory in the code.
+  
+  This code is part of the AutomationShield hardware and software
+  ecosystem. Visit http://www.automationshield.com for more
+  details. This code is licensed under a Creative Commons
+  Attribution-NonCommercial 4.0 International License.
+
+  Created by Gergely Takács. 
+  Last update: 12.10.2018.
+*/
+
+#include <AutomationShield.h> 		// Include the library
+
+unsigned long Ts = 2000;            // Sampling in milliseconds
+unsigned long k = 0;                // Sample index
+bool enable=false;                  // Flag for sampling 
+
+float r = 0.0;						// Reference
+float R[]={40.0,75.0,35.0,70.0,50.0,0.0};;    // Reference trajectory
+int T = 1800;						// Section length (steps) 1 hrs
+int i = i;							// Section counter
+float y = 0.0;						// Output
+float u = 0.0;						// Input					
+
+#define KP 3.0       						// PID Kp
+#define TI 30.0                    // PID Ti
+#define TD 1                    // PID Td
+
+void setup() {
+  Serial.begin(9600);               // Initialize serial
+  
+  // Initialize and calibrate board
+  HeatShield.begin();               // Define hardware pins
+
+  
+  // Initialize sampling function
+  Sampling.interruptInitialize(Ts * 1000);   // Sampling init.
+  Sampling.setInterruptCallback(stepEnable); // Interrupt fcn.
+
+ // Set the PID constants
+ PIDAbs.setKp(KP);
+ PIDAbs.setTi(TI);
+ PIDAbs.setTd(TD); 
 }
 
+// Main loop launches a single step at each enable time
+void loop() {
+  if (enable) { 							// If ISR enables
+    step();									// Algorithm step
+    enable=false;  							// Then disable
+  }  
+}
 
+void stepEnable(){  						// ISR 
+  enable=true;							    // Change flag
+}
+
+// A signle algoritm step
+
+void step(){ 
+
+if (k % (T*i) == 0){				
+  r = R[i]; 							 // Set reference
+  i++;
+}
+  							  
+y = HeatShield.sensorRead();           // Read sensor 
+u = PIDAbs.compute(r-y,0,100,0,100);   // PID
+HeatShield.actuatorWrite(u);           // Actuate
+
+Serial.print(r);						// Print reference
+Serial.print(", ");						 
+Serial.print(y);						// Print output  
+Serial.print(", ");
+Serial.println(u);						// Print input
+k++;									// Increment k
+
+}
--- examples/HeatShield_Random/HeatShield_Random.ino
+++ examples/HeatShield_Random/HeatShield_Random.ino
@@ -2,40 +2,42 @@
   HeatShield system identification experiment
 
   Creates a pulse train to extract input-output data.
-  
-  This example initializes the sampling subsystem from the 
+
+  This example initializes the sampling subsystem from the
   AutomationShield library and starts an open-loop experiment
   with the intention of extracting useful data for a system
   identification procedure. Upload the code to your board, then
   open the Serial Monitor or log the data to an external program.
-  
+
   This code is part of the AutomationShield hardware and software
   ecosystem. Visit http://www.automationshield.com for more
   details. This code is licensed under a Creative Commons
   Attribution-NonCommercial 4.0 International License.
 
-  Created by Gergely Takács. 
+  Created by Gergely Takács.
   Last update: 10.10.2018.
 */
 
 #include <AutomationShield.h> 		// Include the library
 
+long noise;                         // Noise component in input
 unsigned long Ts = 2000;            // Sampling in milliseconds
 unsigned long k = 0;                // Sample index
-bool enable=false;                  // Flag for sampling 
+bool enable = false;                // Flag for sampling
 
 float y = 0.0;						// Output
 float u = 0.0;						// Input (open-loop)
-float U[]={25.0,85.0,50.0,15.0,100.0,0.0};  // Input trajectory
-int T = 1350;						// Section length (45 min)
+float U[] = {25.0, 85.0, 50.0, 15.0, 75.0}; // Input trajectory
+int nU = sizeof(U) / sizeof(U[0]);  // Number of elements
+int T = 1800;						// Section length (60 min)
 int i = 0;							// Section counter
 
 void setup() {
   Serial.begin(9600);               // Initialize serial
-  
+  randomSeed(analogRead(1));        // Seed random numbers
   // Initialize and calibrate board
   HeatShield.begin();               // Define hardware pins
-  
+
   // Initialize sampling function
   Sampling.interruptInitialize(Ts * 1000);   // Sampling init.
   Sampling.setInterruptCallback(stepEnable); // Interrupt fcn.
@@ -44,28 +46,45 @@ void setup() {
 // Main loop launches a single step at each enable time
 void loop() {
   if (enable) { 							// If ISR enables
-    step();									// Algorithm step
-    enable=false;  							// Then disable
-  }  
+    step();									  // Algorithm step
+    enable = false;  					 // Then disable
+  }
 }
 
-void stepEnable(){  						// ISR 
-  enable=true;							    // Change flag
+void stepEnable() {  					// ISR
+  enable = true;							 // Change flag
 }
 
 // A signle algoritm step
-void step(){ 
+void step() {
 
-if (k % (T*i) == 0){				
-  u = U[i]; 							 // Set reference
-  i++;
-}
-  							  
-y = HeatShield.sensorRead();           // Read sensor 
-HeatShield.actuatorWrite(u);           // Actuate
-	 
-Serial.print(y);						// Print output  
-Serial.print(", ");
-Serial.println(u);						// Print input
-k++;									// Increment k
+  if (i <= nU) {                 // If experiment is still on
+    if (k % (T * i) == 0) {      // If new section
+      u = U[i];     						 // Set reference
+      i++;                       // Increment section
+    }
+  }
+  else {								         // Experiment done
+    HeatShield.actuatorWrite(0);    // Turn off heat shield
+    while (1);
+    }
+
+  y = HeatShield.sensorRead();           // Read sensor
+  noise = ((float)random(-50, 50)) / 10;
+  u = u + noise;
+  
+  if (u>=100.0) {  
+	u=100.0;
+  }
+  
+  if (u<=0.0) {  
+	u=0.0;
+  }
+  
+  HeatShield.actuatorWrite(u);           // Actuate
+
+  Serial.print(y);						// Print output
+  Serial.print(", ");
+  Serial.println(u);						// Print input
+  k++;									// Increment k
 }
