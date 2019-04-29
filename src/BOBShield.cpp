cf7c00e49629098e3a8bca917671abd1b5647a8c
corleone68 <35456504+corleone68@users.noreply.github.com>
2019-04-29 21:03:50 +0200

    Update Bobshield.cpp

--- src/BOBShield.cpp
+++ src/BOBShield.cpp
@@ -2,39 +2,25 @@
 
 
 
-/*void BOBClass::begin(long baudRate) {
-  Serial.begin(115200);
-}*/
-
-
 // declaring PIN and initializing sensor library
-void BOBClass::initialize() {
-  pinMode(POT_PIN, INPUT);
-    Serial.println("Adafruit VL6180X test");
-    if (!sens.begin(i2c_addr, _debug)) {
-        Serial.println(F("Failed to boot VL6180X"));
-        while(1);
-    }
-    Serial.println(F("VL6180X API Simple Ranging example\n\n"));
+void BOBClass::begin() {
+  pinMode(POT_PIN, INPUT);			// set Potentiometer pin
 }
 
-void BOBClass::debug() {
-    _debug = true;
-}
 
 void BOBClass::calibration()
 {
 	short calmeasure; 				   		// Temporary measurement value
-	int minCalibrated;						// Actual minimum value
+
 
 	BOBShield.servoWrite(130);					// Tilt beam to the maximum so the ball falls towards the sensor
-	delay(500);					   			    // Wait for things to settle
-	for (int i=1; i<=100; i++) {	    		// Perform 100 measurements
-		calmeasure = BOBShield.sensorRead(); 		// Measure
-		if (calmeasure < minCalibrated){ 		// If lower than already
-			minCalibrated = calmeasure; 		// Save new minimum
+	delay(500);					   		// Wait for things to settle
+	for (int i=1; i<=100; i++) {	    				// Perform 100 measurements
+		calmeasure = BOBShield.sensorRead(); 			// Measure
+		if (calmeasure < minCalibrated){ 			// If lower than already
+			minCalibrated = calmeasure; 			// Save new minimum
 		}
-		delay(10);								// Wait between measurements
+		delay(10);						// Wait between measurements
 	}
 
     calibrated = 1;
@@ -42,15 +28,15 @@ void BOBClass::calibration()
 }
 
 
-//values from potenciometer in %
+//values from potentiometer in %
 float BOBClass::referenceRead(){
    _referenceRead = analogRead(POT_PIN);
    _referenceValue = AutomationShield.mapFloat(_referenceRead,0.00,1023.00,0.00,100.00);
   return _referenceValue;
 }
 
-//values from potenciometer computed for servo
-float BOBClass::servoWrite(){
+//values from potentiometer computed for servo
+float BOBClass::actuatorWrite(){
    _referenceRead = analogRead(POT_PIN);
    _servoValue = AutomationShield.mapFloat(_referenceRead,0.00,1023.00,70.00,130.00);
   return _servoValue;
@@ -59,9 +45,10 @@ float BOBClass::servoWrite(){
 //values from sensor in %
 void BOBClass::sensorRead(){
  range = sens.readRange();
- status = sens.readRangeStatus();
- if (status == VL6180X_ERROR_NONE) {
-   Serial.print("Range: "); Serial.println(range);
+	
+    if (range < minimum) {range = minimum;}
+    else if (range > maximum) {range = lastValue;}
+
 
    pos = map(range,minimum,maximum,100,0);
  return pos();
