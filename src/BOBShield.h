590a841d207a48d67995fa9ec3b6a301bfed7662
corleone68 <35456504+corleone68@users.noreply.github.com>
2019-04-29 21:04:04 +0200

    UpdateBobshield.h

--- src/BOBShield.h
+++ src/BOBShield.h
@@ -15,21 +15,16 @@
 
 class BOBClass {
   public:
-    Adafruit_VL6180X sens = Adafruit_VL6180X();    //symbolic name for sensor library
-    void  initialize(void);                        //sets up sensor
-    void calibration();		                   //calibration
-    float referenceRead();		           // Read reference pot in %
+    Adafruit_VL6180X sens = Adafruit_VL6180X();   	  //symbolic name for sensor library
+    void  begin(void);                      		  //sets up sensor
+    void calibration();		                 	  //calibration
+    float referenceRead();		          	  // Read reference pot in %
     float sensorRead();
-    void debug();
-    float servoWrite();
-
-    //void begin(long baudRate=115200);
+    float actuatorWrite();
 
 
   private:
 
-      uint8_t i2c_addr = 0x29;                  
-      boolean _debug = false;
       float _referenceRead;
       float _referenceValue;
       uint8_t range;
@@ -38,6 +33,7 @@ class BOBClass {
       float _servoValue;
       int pos;
       byte calibrated = 0;
+      int minCalibrated;			// Actual minimum value
       int minimum = 20;                      //TODO: determine what's minimum of the clipping interval
       int maximum = 350;                     //TODO: determine what's maximum of the clipping interval
 
