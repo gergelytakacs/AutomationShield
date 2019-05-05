0c837ea154c0c6e739b9d451ef1954d7040c306d
corleone68 <35456504+corleone68@users.noreply.github.com>
2019-05-05 09:38:59 +0200

    Update Sampling.h

--- src/Sampling.h
+++ src/Sampling.h
@@ -41,15 +41,24 @@ class SamplingClass{
     static void defaultInterrupt();
     p_to_void_func interruptCallback;    
 
-    #ifdef ARDUINO_ARCH_AVR
+    /*#ifdef ARDUINO_ARCH_AVR
 		// Default: Timer1
 		const unsigned long timerResolution = 65536; 					// AVR Timer 1 is 16bit            
-		const unsigned char cpuFrequency = 16; 							// CPU frequency in micro Hertz
+		const unsigned char cpuFrequency = 16; 						// CPU frequency in micro Hertz*/
+	
+    #ifdef ARDUINO_AVR_UNO 	//#if defined(__AVR_ATtiny24__) || defined(__AVR_ATtiny44__) || defined(__AVR_ATtiny84__) || defined(__AVR_ATtiny25__) || defined(__AVR_ATtiny45__) || defined(__AVR_ATtiny85__)	
+		   // Default: Timer2
+		const unsigned long timerResolution = 256; 					// AVR Timer 2 is 8bit            
+		const unsigned char cpuFrequency = 16; 							// CPU frequency in micro Hertz	
+    #elif ARDUINO_AVR_MEGA2560
+        // Default: Timer5
+	    const unsigned long timerResolution = 65536; 					// AVR Timer 5 is 16bit            
+		const unsigned char cpuFrequency = 16; 							// CPU frequency in micro Hertz*/
     #elif ARDUINO_ARCH_SAMD
 		// Default TC5 (randomly selected, take care of Servo!)
 		const unsigned long timerResolution = 65536;     				// Configured to 16bit  
 		const unsigned char cpuFrequency = VARIANT_MCK/1000000; 		// CPU frequency in micro Hertz (48 for Zero)
-  	#elif ARDUINO_ARCH_SAM
+    #elif ARDUINO_ARCH_SAM
 		// Default TC3 (randomly selected, take care of Servo!)
 		const unsigned char cpuFrequency = VARIANT_MCK/1000000; 		// CPU frequency in micro Hertz (84 for Due)
     #else
