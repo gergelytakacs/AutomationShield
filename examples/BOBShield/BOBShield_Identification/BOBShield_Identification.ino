b150c830ef0b33acb6693381dffca3e5310f059b
Corleone <matthew139@gmail.com>
2019-05-08 17:58:07 +0200

    PID and Identification

---
+++ examples/BOBShield/BOBShield_Identification/BOBShield_Identification.ino
@@ -0,0 +1,72 @@
+/*
+  HeatShield system identification experiment
+
+  Creates a pulse train to extract input-output data.
+  
+  This example initializes the sampling subsystem from the 
+  AutomationShield library and starts an open-loop experiment
+  with the intention of extracting useful data for a system
+  identification procedure. Upload the code to your board, then
+  open the Serial Monitor or log the data to an external program.
+  
+  This code is part of the AutomationShield hardware and software
+  ecosystem. Visit http://www.automationshield.com for more
+  details. This code is licensed under a Creative Commons
+  Attribution-NonCommercial 4.0 International License.
+
+  Created by Gergely Takács. 
+  Last update: 10.10.2018.
+*/
+
+#include <BOBShield.h> 		// Include the library
+
+unsigned long Ts = 2000;            // Sampling in milliseconds TODO
+unsigned long k = 0;                // Sample index
+bool enable=false;                  // Flag for sampling 
+
+float y = 0.0;						// Output
+float u = 0.0;						// Input (open-loop)
+float U[]={25.0,85.0,50.0,15.0,100.0,0.0};  // Input trajectory in degree   TODO
+int T = 1350;						// Section length (45 min)  TODO
+int i = 0;							// Section counter
+
+void setup() {
+  Serial.begin(115200);               // Initialize serial
+  
+  // Initialize and calibrate board
+  BOBShield.begin();               // Define hardware pins
+  BOBShield.calibration();         // run calibration
+  
+  // Initialize sampling function
+  Sampling.period(Ts * 1000);   // Sampling init.
+  Sampling.interrupt(stepEnable); // Interrupt fcn.
+}
+
+// Main loop launches a single step at each enable time
+void loop() {
+  if (enable) { 							// If ISR enables
+    step();									// Algorithm step
+    enable=false;  							// Then disable
+  }  
+}
+
+void stepEnable(){  						// ISR 
+  enable=true;							    // Change flag
+}
+
+// A signle algoritm step
+void step(){ 
+
+if (k % (T*i) == 0){				
+  u = U[i]; 							 // Set reference
+  i++;
+}
+  							  
+y = BOBShield.sensorRead();           // Read sensor 
+BOBShield.actuatorWrite(u);           // Actuate
+	 
+Serial.print(y);						// Print output  
+Serial.print(", ");
+Serial.println(u);						// Print input
+k++;									// Increment k
+}
---
+++ examples/BOBShield/BOBShield_PID/BOBShield_PID.ino
@@ -0,0 +1,90 @@
+/*
+  HeatShield closed-loop PID response example
+
+  PID feedback control of the heating block temperature.
+  
+  This example initializes the sampling and PID control 
+  subsystems from the AutomationShield library and starts a 
+  predetermined reference trajectory for the heating block
+  temperature. 
+  
+  Upload the code to your board, then open the Serial
+  Plotter function in your Arduino IDE. You may change the
+  reference trajectory in the code.
+  
+  This code is part of the AutomationShield hardware and software
+  ecosystem. Visit http://www.automationshield.com for more
+  details. This code is licensed under a Creative Commons
+  Attribution-NonCommercial 4.0 International License.
+
+  Created by Gergely Takács. 
+  Last update: 12.10.2018.
+*/
+
+#include <BOBShield.h> 		// Include the library
+
+unsigned long Ts = 2000;            // Sampling in milliseconds
+unsigned long k = 0;                // Sample index
+bool enable=false;                  // Flag for sampling 
+
+float r = 0.0;						// Reference
+float R[]={40.0,75.0,35.0,70.0,50.0,0.0};    // Reference trajectory in mm
+int T = 1800;						// Section length (steps) 1 hrs
+int i = i;							// Section counter
+float y = 0.0;						// Output
+float u = 0.0;						// Input					
+
+#define KP 3.0       						// PID Kp
+#define TI 30.0                    // PID Ti
+#define TD 1                    // PID Td
+
+void setup() {
+  Serial.begin(115200);               // Initialize serial
+  
+  // Initialize and calibrate board
+  BOBShield.begin();               // Define hardware pins
+
+  
+  // Initialize sampling function
+  Sampling.period(Ts * 1000);   // Sampling init.
+  Sampling.interrupt(stepEnable); // Interrupt fcn.
+
+ // Set the PID constants
+ PIDAbs.setKp(KP);
+ PIDAbs.setTi(TI);
+ PIDAbs.setTd(TD); 
+}
+
+// Main loop launches a single step at each enable time
+void loop() {
+  if (enable) { 							// If ISR enables
+    step();									// Algorithm step
+    enable=false;  							// Then disable
+  }  
+}
+
+void stepEnable(){  						// ISR 
+  enable=true;							    // Change flag
+}
+
+// A signle algoritm step
+
+void step(){ 
+
+if (k % (T*i) == 0){				
+  r = R[i]; 							 // Set reference
+  i++;
+}
+  							  
+y = BOBShield.sensorRead();           // Read sensor 
+u = PIDAbs.compute(r-y,0,100,0,100);   // PID
+BOBShield.actuatorWrite(u);           // Actuate
+
+Serial.print(r);						// Print reference
+Serial.print(", ");						 
+Serial.print(y);						// Print output  
+Serial.print(", ");
+Serial.println(u);						// Print input
+k++;									// Increment k
+
+}
