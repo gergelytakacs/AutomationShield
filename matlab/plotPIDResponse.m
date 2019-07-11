2128361ce3b86d594602df10e9660939ed3dbb1c
Peter Chmučiak <51911541+PeterChmurciak@users.noreply.github.com>
2019-07-11 19:28:10 +0200

    Update plotPIDResponse.m
    Updated plotPIDResponse function to work with both .mat and .txt file types.

--- matlab/plotPIDResponse.m
+++ matlab/plotPIDResponse.m
@@ -8,28 +8,40 @@
 %   Attribution-NonCommercial 4.0 International License.
 %
 %   Created by Peter Chmurciak.
-%   Last update: 10.7.2019.
+%   Last update: 11.7.2019.
 
-function plotPIDResponse(aFile, aFig)        % Function definition
-data = load(aFile);                          % Loads data to variable
-r = data.response(:, 1);                     % Separates each column
-y = data.response(:, 2);
-u = data.response(:, 3);
-k = 1:length(r);                             % Vector of sample indexes
+function plotPIDResponse(aFile, aFig)            % Function definition
+data = load(aFile);                              % Loads data to variable
+[fPath, fName, fExt] = fileparts(aFile);         % Analyzes file properties
+switch lower(fExt)                               % Decides based on file type             
+    case '.mat'                                  % If its .mat file
+        matObj = matfile(aFile);
+        details = whos(matObj);
+        r = data.(details.name)(:, 1);           % Separates each column
+        y = data.(details.name)(:, 2);
+        u = data.(details.name)(:, 3);
+    case '.txt'                                  % If its .txt file
+        r = data(:, 1);                          % Separates each column
+        y = data(:, 2);
+        u = data(:, 3);
+    otherwise                                    % If its not .mat or .txt file
+        error('Unexpected file extension: %s', fExt);
+end
+k = 1:length(r);                                 % Vector of sample indexes
 
-maxValue = max([max(r), max(y), max(u)]);    % Finds extreme values
+maxValue = max([max(r), max(y), max(u)]);        % Finds extreme values
 minValue = min([min(r), min(y), min(u)]);
-range = maxValue - minValue;                 % Calculates data range
+range = maxValue - minValue;                     % Calculates data range
 
-figure(aFig)                                                     % Plots on specified figure                                                 
-plot(k, u, 'Color', [0.4660, 0.6740, 0.1880], 'LineWidth', 1)    % Plots input
+figure(aFig)                                                   % Plots on specified figure
+plot(k, u, 'Color', [0.4660, 0.6740, 0.1880], 'LineWidth', 1)  % Plots input
 hold on
-plot(k, y, 'Color', [0.8500, 0.3250, 0.0980], 'LineWidth', 1)    % Plots output
-stairs(k, r, 'Color', [0, 0.4470, 0.7410], 'LineWidth', 1)       % Plots reference
+plot(k, y, 'Color', [0.8500, 0.3250, 0.0980], 'LineWidth', 1)  % Plots output
+stairs(k, r, 'Color', [0, 0.4470, 0.7410], 'LineWidth', 1)     % Plots reference
 hold off
-xlim([k(1), k(end)])                                             % Limits range of x-axis
-ylim([minValue - range / 25, maxValue + range / 25])             % Limits range of y-axis
-title('Plot of system PID response')                             % Figure description
+xlim([k(1), k(end)])                                           % Limits range of x-axis
+ylim([minValue - range / 25, maxValue + range / 25])           % Limits range of y-axis
+title('Plot of system PID response')                           % Figure description
 legend('Input u(k)', 'Output y(k)', 'Reference r(k)', 'Location', 'northwest')
 xlabel('k')
 ylabel('u(k)   y(k)   r(k)')
