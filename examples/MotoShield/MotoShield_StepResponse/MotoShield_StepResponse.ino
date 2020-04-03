5dd15f80a388f9a28f0c90cc2649a12effb97b35
Ján Boldocký <58009306+xboldocky@users.noreply.github.com>
2020-04-03 05:02:54 +0200

    Update MotoShield_StepResponse.ino

--- examples/MotoShield/MotoShield_StepResponse/MotoShield_StepResponse.ino
+++ examples/MotoShield/MotoShield_StepResponse/MotoShield_StepResponse.ino
@@ -1,57 +1,16 @@
-#include "MotoShield.h"
-#include <Sampling.h>            // Include sampling
-   
-   float Setpoint = 100.00;        // reference
-   
-   bool enable=false;              // variables for sampling
-   unsigned long int curTime=0;
-   unsigned long int prevTime=0;
-   
-   unsigned long Ts = 0.1;         // sampling time
-   
-   
-   float senzor = 0.00;            // variables
-   float converted = 0.00;
-   
-                                   // boundaries of converting
-   float maximum = 24.36;          
-   float minimum = 10.27;
-   
-   void setup() {
-   
-   Serial.begin(9600);
-   
-   Sampling.period(Ts * 1000);
-   Sampling.interrupt(stepEnable);
-                                   // sampling
-   
-   MotoShield.begin();             // board initialization   
-   MotoShield.setDirection(true);  // starting the motor
-   MotoShield.setMotorSpeed(Setpoint);
-   }
-   
-   void loop() {
-   
-   curTime=millis();
-   if (enable) {
-   step();
-   enable=false;
-   
-   } // end of the if statement  
-   } // end of the loop
-   
-   void stepEnable(){
-   enable=true;
-   }
-   
-   void step(){
-   senzor = MotoShield.readRevolutions(50);  
-                                   // measuring rpm
-   
-   converted = AutomationShield.mapFloat(senzor,minimum,maximum,0.00,100.00); 
-                                   // converting rpm into %
-   
-   Serial.print(Setpoint);
-   Serial.print(",");
-   Serial.println(converted); 
-   }
+#include <MotoShield.h>
+#define TS 100.0 //--Sampling period in milliseconds
+
+void setup() {
+  Serial.begin(2000000);//--Initialize serial communication # 2 000 000 baudrate
+  MotoShield.begin(TS);//--Initialize MotoShield with sampling period 100 millis
+  MotoShield.calibration();//--Calibration method
+}
+
+void loop() {
+  float r = MotoShield.referenceRead(); //--Reading potentiometers (reference) value in percents
+  MotoShield.actuatorWrite(r); //--Defining potentiometers value as input
+  Serial.print(MotoShield.sensorReadRPMPerc()); //--Printing angular velocity in percents to serial monitor
+  Serial.print(" ");
+  Serial.println(r); //--Printing potentiometers value to serial monitor
+}
