10b96dfa83a9f27c64d81835940a74896e04e8b9
Peter Chmučiak <51911541+PeterChmurciak@users.noreply.github.com>
2020-05-13 15:27:20 +0200

    Updating python C generating function
    Updating python C generating function as I have found out that the generate_c_files method does have the option of saving the files in one directory - what I have done previously manually.

--- examples/FloatShield/FloatShield_MPC/FloatShield_muAO-MPC/generate_C_files.py
+++ examples/FloatShield/FloatShield_MPC/FloatShield_muAO-MPC/generate_C_files.py
@@ -1,14 +1,6 @@
 # This script is used to generate files containing C code for FloatShield Arduino IDE MPC example
 import muaompc  # Import muaompc module (http://ifatwww.et.uni-magdeburg.de/syst/muAO-MPC/)
-import os   # Import os module required for rearranging the compiled files
 
 # Note: the "numpy" module is required to run this script
 mpc = muaompc.ltidt.setup_mpc_problem('model_parameters')  # Load model parameters from the specified file
-mpc.generate_c_files(numeric='float32')  # Generate respective C files, use float type for its variables
-
-# An auxiliary routine to rearrange the compiled files
-list_of_headers = os.listdir('cmpc/include')
-for header in list_of_headers:
-    os.replace('cmpc/include/' + header, 'cmpc/' + header)
-os.remove('cmpc/Makefile')
-os.rmdir('cmpc/include')
+mpc.generate_c_files(numeric='float32', singledir=True)  # Generate respective C files, use float type for its variables
