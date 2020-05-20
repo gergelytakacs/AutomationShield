a16b0f5d30ae8af510ebdb69fd4f4457de0958af
Ján Boldocký <58009306+xboldocky@users.noreply.github.com>
2020-05-21 01:57:07 +0200

    Update MotoShield_Identification.ino

--- examples/MotoShield/MotoShield_Identification/MotoShield_Identification.ino
+++ examples/MotoShield/MotoShield_Identification/MotoShield_Identification.ino
@@ -1,36 +1,64 @@
-#include <MotoShield.h>     //--Including API
-#define TS 40.0            //--Defining Sample period in milliseconds
+/*
+  MotoShield Identification data acquisition.
 
-unsigned int k = 0;                //--Sample index
-float y = 0.0;                    //--Output
-float u = 0.0;                   //--Input
-float U[]={40.0,70.0,50.0,85.0,35.0,60.0};  //--Input trajectory
-int T = 80;              //--Section length
-int i = i;               //--Section counter
+  This code is part of the AutomationShield hardware and software
+  ecosystem. Visit http://www.automationshield.com for more
+  details. This code is licensed under a Creative Commons
+  Attribution-NonCommercial 4.0 International License.
+
+  Created by Ján Boldocký.
+  Last update: 23.4.2020.
+*/
+#include<MotoShield.h> //--Include API
+#define TS 20.0 //--Sampling period # symbolic constant
+
+#define PRBS 0 //--APRBS data selected
+
+#if PRBS
+#include "prbsU.h"
+float prbs;
+#else
+#include "aprbsU.h"
+float aprbs;
+#endif
+int i;
+float u;
 
 void setup() {
-  Serial.begin(9600);               //--Initialize serial communication # 9.6 Kbaud
-  MotoShield.begin(TS);            //--Initialize MotoShield # input argument -> Sample period
-  MotoShield.calibration();       //--Calibration
+  Serial.begin(500000); //--initialize Serial
+  MotoShield.begin(TS); //--initialize MotoShield
 }
 
-void loop() { 
-  if (MotoShield.stepEnable) {    //--Running the algorithm once every sample     
-    step();                
-    MotoShield.stepEnable=false; //--Setting the flag to false # built-in ISR sets flag to true at the end of each sample    
-  }  
-}
-
-void step(){          //--Algorith ran once per sample
-  if (k % (T*i) == 0){ //--Moving through trajectory values    
-    u = U[i];         
-    i++;             //--Change input value after defined amount of samples
+void loop() {
+  if (MotoShield.stepEnable) {//--Flag condition 
+    step();     //--algorithm
+    MotoShield.stepEnable = false; //--disable Flag
   }
-                  
-  y = MotoShield.sensorReadRPMPerc();   //--Sensing angular velocity in percent
-  MotoShield.actuatorWrite(u);         //--Actuation
-  Serial.print(y);                    //--Printing output
-  Serial.print(" ");
-  Serial.println(u);                 //--Printing input
-  k++;                              //--Increment
+}
+
+void step() { //--Algorythm
+#if PRBS  //--case PRBS 
+  if (i > prbsU_length) { //--end of data
+    MotoShield.actuatorWrite(0); //--Stop the Motor 
+    while (1); //--Stop the program
+  }
+  else { 
+    u = pgm_read_word(&prbsU[i]); //--read actuating value
+  }
+#else //--Case ARPBS
+  if (i > aprbsU_length) { //--end of data
+    MotoShield.actuatorWrite(0); //--Stop the Motor
+    while (1); //--Stop the program
+  }
+  else {
+    u = pgm_read_float_near(&aprbsU[i]); //--read actuating value
+  }
+#endif
+MotoShield.actuatorWriteVolt(u); //--Actuate
+Serial.print(u,5); //--Print actuating value
+Serial.print(", "); //--CSV
+Serial.print(MotoShield.counted); //--Print Number of ticks per sample 
+Serial.print(", ");
+Serial.println(MotoShield.sensorReadCurrent(),5); //--Print Current
+i++; //--Sample #data# index
 }
