9cda13ef83398f81e853e931afc29b2750b13ee0
Ján Boldocký <58009306+xboldocky@users.noreply.github.com>
2020-05-21 00:26:15 +0200

    Update MotoShield_OpenLoop.ino

--- examples/MotoShield/MotoShield_OpenLoop/MotoShield_OpenLoop.ino
+++ examples/MotoShield/MotoShield_OpenLoop/MotoShield_OpenLoop.ino
@@ -1,13 +1,22 @@
+/*
+  MotoShield API exaple - Open loop.
+
+  This code is part of the AutomationShield hardware and software
+  ecosystem. Visit http://www.automationshield.com for more
+  details. This code is licensed under a Creative Commons
+  Attribution-NonCommercial 4.0 International License.
+
+  Created by Ján Boldocký.
+  Last update: 23.4.2020.
+*/
 #include <MotoShield.h>
 #define TS 100.0 //--Sampling period in milliseconds
-float r;
-
 void setup() {
   Serial.begin(2000000);//--Initialize serial communication # 2 000 000 baudrate
   MotoShield.begin(TS);//--Initialize MotoShield with sampling period 100 millis
   MotoShield.calibration();//--Calibration method
 }
-
+  float r;
 void loop() {
   r = MotoShield.referenceRead(); //--Reading potentiometers (reference) value in percents
   MotoShield.actuatorWrite(r); //--Defining potentiometers value as input
