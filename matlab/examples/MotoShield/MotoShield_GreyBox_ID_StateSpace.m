eb5dd54fc5b008849404a98f7157b182faf84c68
Ján Boldocký <58009306+xboldocky@users.noreply.github.com>
2020-05-22 20:06:50 +0200

    Update MotoShield_GreyBox_ID_StateSpace.m

--- matlab/examples/MotoShield/MotoShield_GreyBox_ID_StateSpace.m
+++ matlab/examples/MotoShield/MotoShield_GreyBox_ID_StateSpace.m
@@ -7,7 +7,7 @@
 %   details. This code is licensed under a Creative Commons
 %   Attribution-NonCommercial 4.0 International License.
 % 
-%   Created by Gergely Takács and Ján Boldocký. 
+%   Created by Gergely TakÃ¡cs and JÃ¡n BoldockÃ½. 
 
 clc; clear; close all;
 
@@ -28,15 +28,14 @@ data.TimeUnit = 's';                            % Time unit
 %% Initial guess of model parameters
 J = 0.01;       %-Moment of inertia
 b = 0.1;        %-Viscous friction constant
-Ke = 0.007;     %-Electromotive force constant # Voltage over Velocity
-Kt = 0.007;     %-Motor torque constant # Torque over Current
+Ke = 1;     %-Electromotive force constant # Voltage over Velocity
+Kt = 1;     %-Motor torque constant # Torque over Current
 R = 10;         %-Electric resistance
-L = 0.5;        %-Electric inductance
+L = 3;        %-Electric inductance
 
 dtheta0  = data.y(1,1);        %-Initial velocity
 i0 = data.y(1,2);        %-Initial current
 
-%% Choose the type of grey-box model to identify
     % Linear SS Model
     A = [-b/J      Kt/J;                       % State-transition matrix
          -Kt/L     -R/L];
@@ -52,13 +51,11 @@ i0 = data.y(1,2);        %-Initial current
     x0 = [dtheta0; i0];                        % Initial condition
     disp('Initial guess:')
     sys = idss(A,B,C,D,K,x0,0)                 % Construct state-space representation
-
     % Mark the free parameters
     sys.Structure.A.Free = [1   1;             % Free and fixed variables
                             1   1];        
     sys.Structure.B.Free = [0  1]';            % Free and fixed variables # Transposed
     sys.Structure.C.Free = false;              % No free parameters
-
     sys.DisturbanceModel = 'estimate';         % Estimate disturbance model
     sys.InitialState = 'estimate';             % Estimate initial states
 
@@ -66,33 +63,12 @@ i0 = data.y(1,2);        %-Initial current
     Options = ssestOptions;                    % State-space estimation options
     Options.Display = 'on';                    % Show progress
     Options.Focus = 'simulation';              % Focus on simulation
-                                               % 'stability'
+    Options.SearchOptions.MaxIterations = 50;
     Options.InitialState = 'estimate';         % Estimate initial condition
-    
+    disp('Estimated model:')                        % Identified model    
     % Identify model
     model = ssest(data,sys,Options);           % Run estimation
-    
-    case 'linearTF'
-    
-    % Creating model structure
-   
-b0= Kt;                       % Gain
-a2=  J*L;                                     % Polynomial coefficients
-a1= (J*R+b*L);
-a0= (b*R+Kt^2);
 
-disp('Initial model:')
-sys = idtf([b0],[a2 a1 a0])                   % Initial guess of the model
-
-%Configure estimation procedure
-Options = tfestOptions;                         % Identification options for ssest
-Options.Display = 'on';                         % Show progress
-Options.EnforceStability = 1;                   % Unstable models allowed
-%Options.InitialCondition = 'estimate';          % Estimate initial condition as well
-
- %Identify model
-disp('Estimated model:')                        % Identified model
-model=tfest(data,sys,Options)                  % Grey box TF
 compare(data,model) 
 grid on                                        % Turn on grid
 
