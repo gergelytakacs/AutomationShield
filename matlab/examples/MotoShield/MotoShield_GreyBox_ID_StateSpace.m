5b4b89dae8e7e9230d0883c29ad2231dac82a38d
Ján Boldocký <58009306+xboldocky@users.noreply.github.com>
2020-05-22 22:27:56 +0200

    Update MotoShield_GreyBox_ID_StateSpace.m

--- matlab/examples/MotoShield/MotoShield_GreyBox_ID_StateSpace.m
+++ matlab/examples/MotoShield/MotoShield_GreyBox_ID_StateSpace.m
@@ -12,31 +12,31 @@
 clc; clear; close all;
 
 %% Data preprocessing
-load aprbsResult.mat                               % Read identificaiton results
-Ts = 0.02;                                     % Sampling
-data = iddata([y ifltr],u,Ts,'Name','Experiment');      % Create identification data object
+load aprbsResult.mat                               %_Load data
+Ts = 0.02;                                     %_Sample time
+data = iddata([y ifltr],u,Ts,'Name','Experiment');      %_Identification data object
 data = detrend(data);                           % Remove offset and linear trends
-data.InputName = 'Input Voltage';                   % Input name
-data.InputUnit =  'V';                          % Input unit
-data.OutputName{1} = 'Angular Velocity';              % Output name
-data.OutputUnit{1} = 'rad/s';                          % Output unit
-data.OutputName{2} = 'Current';              % Output name
-data.OutputUnit{2} = 'A';                          % Output unit
-data.Tstart = 0;                                % Starting time
-data.TimeUnit = 's';                            % Time unit
+data.InputName = 'Input Voltage';                   %_Input name
+data.InputUnit =  'V';                          %_Input unit
+data.OutputName{1} = 'Angular Velocity';              %_Output name
+data.OutputUnit{1} = 'rad/s';                          %_Output unit
+data.OutputName{2} = 'Current';              %_Output name
+data.OutputUnit{2} = 'A';                          %_Output unit
+data.Tstart = 0;                                %_Starting time
+data.TimeUnit = 's';                            %_Time unit
 
 %% Initial guess of model parameters
-J = 0.01;       %-Moment of inertia
-b = 0.1;        %-Viscous friction constant
-Ke = 1;     %-Electromotive force constant # Voltage over Velocity
-Kt = 1;     %-Motor torque constant # Torque over Current
-R = 10;         %-Electric resistance
-L = 3;        %-Electric inductance
+J = 0.01;       %_Moment of inertia
+b = 0.1;        %_Viscous friction constant
+Ke = 1;     %_Electromotive force constant # Voltage over Velocity
+Kt = 1;     %_Motor torque constant # Torque over Current
+R = 10;         %_Electric resistance
+L = 3;        %_Electric inductance
 
-dtheta0  = data.y(1,1);        %-Initial velocity
-i0 = data.y(1,2);        %-Initial current
+dtheta0  = data.y(1,1);        %_Initial velocity
+i0 = data.y(1,2);        %_Initial current
 
-    % Linear SS Model
+    %_StateSpace Model
     A = [-b/J      Kt/J;                       % State-transition matrix
          -Kt/L     -R/L];
      
@@ -48,28 +48,26 @@ i0 = data.y(1,2);        %-Initial current
     D = [0;
          0];                                   % No direct feed-through                                            
     K = zeros(2,2);                            % Disturbance
-    x0 = [dtheta0; i0];                        % Initial condition
+    x0 = [dtheta0; i0];                        %_Initial condition
     disp('Initial guess:')
-    sys = idss(A,B,C,D,K,x0,0)                 % Construct state-space representation
-    % Mark the free parameters
-    sys.Structure.A.Free = [1   1;             % Free and fixed variables
+    sys = idss(A,B,C,D,K,x0,0)                 %_Initial State Space Model
+    sys.Structure.A.Free = [1   1;             %_A variables - free
                             1   1];        
-    sys.Structure.B.Free = [0  1]';            % Free and fixed variables # Transposed
-    sys.Structure.C.Free = false;              % No free parameters
+    sys.Structure.B.Free = [0  
+                            1];                %_B variables
+    sys.Structure.C.Free = false;              %_C fixed
     sys.DisturbanceModel = 'estimate';         % Estimate disturbance model
     sys.InitialState = 'estimate';             % Estimate initial states
-
-    % Set identification options
+    
     Options = ssestOptions;                    % State-space estimation options
     Options.Display = 'on';                    % Show progress
     Options.Focus = 'simulation';              % Focus on simulation
-    Options.SearchOptions.MaxIterations = 50;
+    Options.SearchOptions.MaxIterations = 50;  % Max iterations
     Options.InitialState = 'estimate';         % Estimate initial condition
-    disp('Estimated model:')                        % Identified model    
-    % Identify model
-    model = ssest(data,sys,Options);           % Run estimation
+    disp('Estimated model:')                       
+    model = ssest(data,sys,Options);           % Identified Model
 
-compare(data,model) 
-grid on                                        % Turn on grid
+compare(data,model)                            % Data/Model Comparison
+grid on                                       
 
 save MotoShield_GreyboxModel_LinearSS model
