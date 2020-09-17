47ff4c9cd65e3524dd2471d1815f9745e6667d79
Gabor7669 <gaborpenzinger94@gmail.com>
2020-09-17 09:33:25 +0200

    Update MagnetoShield_LQ.ino

--- examples/MagnetoShield/MagnetoShield_LQ/MagnetoShield_LQ.ino
+++ examples/MagnetoShield/MagnetoShield_LQ/MagnetoShield_LQ.ino
@@ -1,6 +1,6 @@
 #include <MagnetoShield.h>
 #include <Sampling.h>
-                              // Include template function for calculating K gain for LQ control applications
+#include <BasicLinearAlgebra.h>                                   // Include template function for calculating K gain for LQ control applications
 
 #define MANUAL 0
 
@@ -19,7 +19,10 @@ bool realTimeViolation=false;         // Flag for real-time violations
 int   i = 0;                          // Experiment section counter
 float y = 0.0;                        // [mm] Output
 float u = 0.0;                        // [V] Input          
-
+uint8_t x1_p;
+float y0= 14.3;                                                       
+float i0= 0.0219;                                                     
+float u0= 4.6234; 
 #ifdef ARDUINO_ARCH_AVR
   unsigned long Ts = 3250;                // Sampling in microseconds, lower limit near 3.2 ms
   int T = 1500;                           // Experiment section length (steps) 
@@ -32,18 +35,18 @@ float u = 0.0;                        // [V] Input
 #endif
 
 // Linear, discrete state-space matrices for FloatShield model
-BLA::Matrix<3, 3> A = {1.0315, 0.0051, -0.0021, 12.6889, 1.0384, -0.6398, -0.0533, -0.0064, 0.1027}; // State matrix A
-BLA::Matrix<3, 1> B = {0.00000922, -0.0048, 0.0044};                              // Input matrix B
-BLA::Matrix<1, 3> C = {1, 0, 0};                                                // Output matrix C
-
+BLA::Matrix<3, 3> A = {1.0133, 0.00327, -0.00109, 8.1875, 1.0168, -0.54516, -0.02724, -0.00542, 0.22523};
+BLA::Matrix<3, 1> B = {-0, -0.00245, 0.00377};
+BLA::Matrix<1, 3> C = {1, 0, 0};
 // Kalman process and measurement error covariances
-BLA::Matrix<4, 4> Q_Kalman = {100, 0, 0, 0, 0, 10, 0, 0, 0, 0, 10, 0, 0, 0, 0, 1};     // Process noise covariance matrix
-BLA::Matrix<1, 1> R_Kalman = {0.0001};                                  // Measurement noise covariance matrix
-
+BLA::Matrix<3, 3> Q_Kalman = {0.0001, 0, 0, 0, 100, 0, 0, 0, 1};
+BLA::Matrix<1, 1> R_Kalman = {0.001};
 // LQ gain with integrator
-BLA::Matrix<1, 4> K = {387.74,-7546.3 , -163.98, 99.65};         // Pre-calculated LQ gain K
-BLA::Matrix<4, 1> X = {0, 0, 0};                                 // Estimated state vector
-BLA::Matrix<4, 1> Xr = {0, 0, 0}; 
+BLA::Matrix<1, 4> K = {163.99, -7799.5, -214.59, 88.944};
+BLA::Matrix<4, 1> X = {0, 0.001, 0, 0};                                 // Estimated state vector
+BLA::Matrix<4, 1> Xr = {0, 0, 0, 0}; 
+
+
 void setup() {
 #if ARDUINO_ARCH_AVR || ARDUINO_ARCH_SAMD
     Serial.begin(2000000);                 // Initialize serial, maximum for AVR given by hardware    
@@ -72,7 +75,7 @@ void stepEnable(){                      // ISR
   if(enable){                           // If previous still running
     realTimeViolation=true;             // RT violated
     Serial.println("Real-time samples violated.");
-    while(1);                           // Stop execution
+    //while(1);                           // Stop execution
   }                                     // else
   enable=true;                          // Change flag
 }
@@ -83,7 +86,7 @@ void step(){
 #if MANUAL                              // If reference from pot
   Xr(0) = AutomationShield.mapFloat(MagnetoShield.referenceRead(),0.0,100.0,12.0,17.0);
 #else                                   // If pre-set experiment
-  if (i>sizeof(R)/sizeof(R[0])){        // If experiment finished
+ if (i>sizeof(R)/sizeof(R[0])){        // If experiment finished
     realTimeViolation=false; 
     MagnetoShield.actuatorWrite(0);     // then turn off magnet
     while(1);                           // and stop
@@ -95,15 +98,38 @@ void step(){
 #endif
 
 // Control algorithm
-y = MagnetoShield.sensorRead();         // [mm] sensor read
-u = -(K * (X - Xr))(0);     
-MagnetoShield.actuatorWrite(u);         // [V] actuate
-MagnetoShield.getKalmanEstimate(X, u, y, A, B, C, Q_Kalman, R_Kalman); 
+X(1) = (float)((MagnetoShield.sensorRead()-y0)/1000.0f);
+X(3) = (float)(MagnetoShield.auxReadCurrent()/1000.0f - i0);  // [mA] Current read)// [mm] sensor read
+float xr = (float)((Xr(0)-y0)/1000.0f);
+X(2) = (X(1) - x1_p)/0.003250;
+
+u = -(K(0)*X(0) + K(1)*X(1) + K(2)*X(2) + K(3)*X(3)) + u0;
+X(0) = X(0) + (xr - X(1)); 
+x1_p = X(1);
+
+Serial.println(X(2),5);
+
+if(u > 12)
+  u=12;
+else if (u < 0)
+  u = 0;   
+MagnetoShield.actuatorWrite(u); // [V] actuate
+#ifdef USE_KALMAN_FILTER
+  MagnetoShield.getKalmanEstimate(X, u, y/1000, A, B, C, Q_Kalman, R_Kalman); 
+#else
+  
+#endif
+ 
 // Print to serial port
-Serial.print(Xr(0));                        // [mm] Print reference
+/*Serial.print(X(0),5);                        // [mm] Print reference
 Serial.print(", ");            
-Serial.print(y);                        // [mm ]Print output  
+Serial.print(x1,5);                        // [mm ]Print output  
 Serial.print(", ");
-Serial.println(u);                      // [V] Print input
+Serial.print(x2,5);                        // [mm ]Print output  
+Serial.print(", ");
+Serial.print(x3,5);                        // [mm ]Print output  
+Serial.print(", ");
+Serial.println(u);*/ // [V] Print input
 k++;                                    // Increment time-step k
+
 }
