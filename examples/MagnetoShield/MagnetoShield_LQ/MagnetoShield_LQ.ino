99c31a6b85696bd31d057e0ef0dc2fd066ebbca4
Gabor7669 <gaborpenzinger94@gmail.com>
2020-09-18 10:41:49 +0200

    Update MagnetoShield_LQ.ino

--- examples/MagnetoShield/MagnetoShield_LQ/MagnetoShield_LQ.ino
+++ examples/MagnetoShield/MagnetoShield_LQ/MagnetoShield_LQ.ino
@@ -15,37 +15,33 @@ bool enable=false;                    // Flag for sampling
 bool realTimeViolation=false;         // Flag for real-time violations
 
 
+float Ref;
+float y = 0.0;                      // [mm] Output (Current object height)
+float yp= 0.0;
+float u = 0.0;                      // [V] Input (Magnet voltage)
+float I = 0.0;                      // [mA] Input (Magnet current)
 
-int   i = 0;                          // Experiment section counter
-float y = 0.0;                        // [mm] Output
-float u = 0.0;                        // [V] Input          
-uint8_t x1_p;
-float y0= 14.3;                                                       
-float i0= 0.0219;                                                     
-float u0= 4.6234; 
-#ifdef ARDUINO_ARCH_AVR
-  unsigned long Ts = 3250;                // Sampling in microseconds, lower limit near 3.2 ms
+float y0= 14.3;                     // [mm] Linearization point based on the experimental identification
+float I0= 21.9;                     // [mA] Linearization point based on the experimental identification
+float u0= 4.6234;                   // [V] Linearization point based on the experimental identification
+
+int i = 0;                          // Section counter
+
+#if ARDUINO_ARCH_AVR
+float Ts = 5;                // Sampling in microseconds, lower limit near 5 ms
   int T = 1500;                           // Experiment section length (steps) 
 #elif ARDUINO_ARCH_SAMD
-  unsigned long Ts = 4200;                 // Sampling in microseconds, lower limit 
+float Ts = 4.200;                 // Sampling in microseconds, lower limit 
   int T = 1500;                            // Experiment section length (steps) 
 #elif ARDUINO_ARCH_SAM
-  unsigned long Ts = 1300;                 // Sampling in microseconds, lower limit 1.3 ms
+ float Ts = 1.300;                 // Sampling in microseconds, lower limit 1.3 ms
   int T = 3000;                            // Experiment section length (steps) 
 #endif
 
-// Linear, discrete state-space matrices for FloatShield model
-BLA::Matrix<3, 3> A = {1.0133, 0.00327, -0.00109, 8.1875, 1.0168, -0.54516, -0.02724, -0.00542, 0.22523};
-BLA::Matrix<3, 1> B = {-0, -0.00245, 0.00377};
-BLA::Matrix<1, 3> C = {1, 0, 0};
-// Kalman process and measurement error covariances
-BLA::Matrix<3, 3> Q_Kalman = {0.0001, 0, 0, 0, 100, 0, 0, 0, 1};
-BLA::Matrix<1, 1> R_Kalman = {0.001};
 // LQ gain with integrator
-BLA::Matrix<1, 4> K = {163.99, -7799.5, -214.59, 88.944};
-BLA::Matrix<4, 1> X = {0, 0.001, 0, 0};                                 // Estimated state vector
+BLA::Matrix<4, 1> K = {115.458, -4666.8, -101.15, 37.033};
+BLA::Matrix<4, 1> X = {0, 0, 0, 0};                                 // Estimated state vector
 BLA::Matrix<4, 1> Xr = {0, 0, 0, 0}; 
-
 
 void setup() {
 #if ARDUINO_ARCH_AVR || ARDUINO_ARCH_SAMD
@@ -59,7 +55,7 @@ void setup() {
   MagnetoShield.calibration();         // Calibrates shield 
      
   // Initialize sampling function
-  Sampling.period(Ts);    // Sampling init.
+  Sampling.period(Ts*1000);    // Sampling init.
   Sampling.interrupt(stepEnable); // Interrupt fcn.
 
 }
@@ -75,7 +71,7 @@ void stepEnable(){                      // ISR
   if(enable){                           // If previous still running
     realTimeViolation=true;             // RT violated
     Serial.println("Real-time samples violated.");
-    //while(1);                           // Stop execution
+    while(1);                           // Stop execution
   }                                     // else
   enable=true;                          // Change flag
 }
@@ -84,7 +80,7 @@ void step(){
 
 // Reference source
 #if MANUAL                              // If reference from pot
-  Xr(0) = AutomationShield.mapFloat(MagnetoShield.referenceRead(),0.0,100.0,12.0,17.0);
+  Xr[0] = AutomationShield.mapFloat(MagnetoShield.referenceRead(),0.0,100.0,12.0,17.0);
 #else                                   // If pre-set experiment
  if (i>sizeof(R)/sizeof(R[0])){        // If experiment finished
     realTimeViolation=false; 
@@ -92,44 +88,35 @@ void step(){
     while(1);                           // and stop
   }
   else if (k % (T * i) == 0){             // else for each section
-    Xr(0) = R[i];                           // set reference
+    Xr(1) = (R[i]-y0)/1000.0;            // set reference
+    Ref = R[i];
     i++;                                // and increase section counter
   }
 #endif
 
 // Control algorithm
-X(1) = (float)((MagnetoShield.sensorRead()-y0)/1000.0f);
-X(3) = (float)(MagnetoShield.auxReadCurrent()/1000.0f - i0);  // [mA] Current read)// [mm] sensor read
-float xr = (float)((Xr(0)-y0)/1000.0f);
-X(2) = (X(1) - x1_p)/0.003250;
+
+y = MagnetoShield.sensorRead();
+float I = MagnetoShield.auxReadCurrent();
+X(0) = X(0) + (Xr(1)- X(1)); 
+X(1) = (y-y0)/1000.0;
+X(2) = (y - yp)/(1000.0*(float(Ts)/1000.0));
+X(3) = (I-I0)/1000.0;  // [mA] Current read)// [mm] sensor read
+
+yp = y;
 
 u = -(K(0)*X(0) + K(1)*X(1) + K(2)*X(2) + K(3)*X(3)) + u0;
-X(0) = X(0) + (xr - X(1)); 
-x1_p = X(1);
-
-Serial.println(X(2),5);
-
-if(u > 12)
-  u=12;
-else if (u < 0)
-  u = 0;   
+u = AutomationShield.constrainFloat(u,0,10);  
 MagnetoShield.actuatorWrite(u); // [V] actuate
-#ifdef USE_KALMAN_FILTER
-  MagnetoShield.getKalmanEstimate(X, u, y/1000, A, B, C, Q_Kalman, R_Kalman); 
-#else
-  
-#endif
- 
-// Print to serial port
-/*Serial.print(X(0),5);                        // [mm] Print reference
-Serial.print(", ");            
-Serial.print(x1,5);                        // [mm ]Print output  
-Serial.print(", ");
-Serial.print(x2,5);                        // [mm ]Print output  
-Serial.print(", ");
-Serial.print(x3,5);                        // [mm ]Print output  
-Serial.print(", ");
-Serial.println(u);*/ // [V] Print input
+
+
+Serial.print(Ref);       // Print reference
+Serial.print(" ");
+Serial.print(y);           // Print output
+Serial.print(" ");
+Serial.print(X(0));           // Print output
+Serial.print(" ");
+Serial.println(u); // [V] Print input
 k++;                                    // Increment time-step k
 
 }
