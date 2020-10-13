8780066616b4e43d5eccd4834112e7b7d3b1baa2
KuboJakub <38165551+KuboJakub@users.noreply.github.com>
2020-10-13 22:27:51 +0200

    Simulink MagnetoLibrari
    Magneto library for simulink - done
    Examples - PID for uno,mega,due
    Linearization and constants calculation for matlab - coil turns, symbolic linearization of Non-lin model

---
+++ matlab/examples/MagnetoShield/MagnetoShield_Model_Linearization.m
@@ -0,0 +1,20 @@
+%% Linearization
+syms x1 x2 x3 x01 x02 x03 R L K u g m C s Ke Km
+
+x = [x1; x2; x3]; % System state variables [position; velocity; current]
+x0 = [x01; x02; x03]; % Linearization points
+
+% Non-linear system equations
+dx1 = x2;
+dx2 = g-Km/m*x3^2/x1^2;
+dx3 = -R/L*x3-2*Ke*x3/(L*x1^2)*x2+1/L*u;
+
+% Jacobian matrix of partial derivatives of the system
+J = jacobian([dx1 dx2 dx3],[x1 x2 x3]);
+
+% Matrix A of the linear SS system
+A = subs(J,x,x0)
+% Matrix B of the linear SS system
+B = [diff(dx1,u);diff(dx2,u);diff(dx3,u)]
+
+
