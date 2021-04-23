8c6d5bb5aa1cb0fc650e7ebf50647ee3addfb830
Gergely Takács <37662064+gergelytakacs@users.noreply.github.com>
2021-04-23 10:08:22 +0200

    Update ectrl.py

--- matlab/examples/MagnetoShield/ectrl.py
+++ matlab/examples/MagnetoShield/ectrl.py
@@ -1,7 +1,7 @@
 """
     MATRICES FOR EXPLICIT MPC POLYTOPES AND CORRESPONDING PWA LAWS
 
- 	  The automatically generated set of matrices have been adapted from 
+ 	The automatically generated set of matrices have been adapted from 
     the C code auto-generation routine of the Multi-Parametric Toolbox 3 
     MPT3) of Kvasnica et al. for  MATLAB. Please see http://www.mpt3.org 
     for more information. Please see the original "toC.m" for more details
@@ -27,7 +27,7 @@
 
 
     Copyright (C) 2005 by Michal Kvasnica (michal.kvasnica@stuba.sk) 
-    Revised in 2012-2013 by Martin Herceg (herceg@control.ee.ethz.ch)    
+    Revised in 2012-2013 by Martin Herceg (herceg@control.ee.ethz.ch)
     Adapted for MCU use by Gergely Takács in 2020 (gergely.takacs@stuba.sk)
 """
 
@@ -261,4 +261,3 @@ MPT_G = [
 -4.62340000000000e+00,	2.44840527062965e+00,	-2.55452580515119e+06,	1.35279544172938e+06,	-2.22366580804131e+00,	
 5.37660000000000e+00,	2.55452580515119e+06,	5.37660000000000e+00,	1.28461139314128e+09,	5.37660000000000e+00,	
 -4.62340000000000e+00,	5.37660000000000e+00,	-2.55452580515119e+06,	5.37660000000000e+00 ]
-
