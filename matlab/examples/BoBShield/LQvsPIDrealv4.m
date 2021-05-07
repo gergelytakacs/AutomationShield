f00222fb83ff7525f12b4f03ac579b52ed1d16e5
Gergely Takács <37662064+gergelytakacs@users.noreply.github.com>
2021-05-07 14:14:50 +0200

    Capitalizing correction

--- matlab/examples/BoBShield/LQvsPIDrealv4.m
+++ matlab/examples/BoBShield/LQvsPIDrealv4.m
@@ -2,7 +2,7 @@ startScript;                            % Clears screen and variables, except al
 
 %define LQR
 Ts=0.01;
-load('mymodel.mat')
+load('myModel.mat')
 csystem=ss(model.A,model.B,model.C,model.D);
 %ssest, so sumom alebo bez, ale rovnako v simulacii ako v modeli. 
 %fok vagy radian? 
