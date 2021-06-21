4ad098198174fdde4a805129fb17b874da12c6a6
Gergely Takács <37662064+gergelytakacs@users.noreply.github.com>
2021-06-21 15:31:52 +0200

    Deleting extra lines to conform to codefactor

--- src/FloatShield.cpp
+++ src/FloatShield.cpp
@@ -125,7 +125,6 @@ void FloatClass::dacWrite(uint16_t DAClevel){	// 16 bits in the form (0,0,0,0,D1
 }
 
 float FloatClass::actuatorReadVoltage(void) {                                                     //measure the actual voltage at the actuator
-
   _actuatorVoltageADC = (float)analogRead(FLOAT_AVPIN);                                           // read the ADC value
   _actuatorVoltage = 4.0 * AutomationShield.mapFloat(_actuatorVoltageADC, 0.0, ADCREF, 0.0, 3.3); // calculate the Voltage form ADC value
   return _actuatorVoltage;
@@ -133,7 +132,6 @@ float FloatClass::actuatorReadVoltage(void) {                                   
 #endif
 
 void FloatClass::actuatorWrite(float aPercent) {
-  
   #if SHIELDRELEASE == 1 || SHIELDRELEASE == 2 || SHIELDRELEASE == 3                     // Write actuator
   float mappedValue = AutomationShield.mapFloat(aPercent, 0.0, 100.0, 0.0, 255.0);       // Takes the float type percentual value 0.0-100.0 and remapps it to range 0.0-255.0
   mappedValue = AutomationShield.constrainFloat(mappedValue, 0.0, 255.0);                // Constrains the remapped value to fit the range 0.0-255.0 - safety precaution
