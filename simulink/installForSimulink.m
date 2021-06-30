a18cbabb7509d3aadce6dbdc0ef8b077dd992417
Gergely Takács <37662064+gergelytakacs@users.noreply.github.com>
2021-06-30 10:36:28 +0200

    FloatLibrary for 2019a and Install script

--- simulink/installForSimulink.m
+++ simulink/installForSimulink.m
@@ -6,11 +6,15 @@
 %   Attribution-NonCommercial 4.0 International License.
 % 
 %   Created by Gergely Takács. 
-%   Last update: 17.10.2018.
+%   Last update: 30.6.2021.
 
 function installAutomationShield()
+    if(verLessThan('Simulink','9.4'))                           % Checking for Simulink version
+        error('Please install MATLAB/Simulink 2019a or newer! The AutomationShield Simulink API is not supported by your MATLAB/Simulink installation.') % Simulink installation incompatible
+    end
     thisdir=pwd;
     addpath(genpath(thisdir));
     savepath
     disp('AutomationShield Simulink API added to MATLAB path.')
+
 end
