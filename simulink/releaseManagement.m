3441c98e73dc8801dc2d4b4c510b43542258a612
Gergely Takács <37662064+gergelytakacs@users.noreply.github.com>
2021-06-30 13:21:38 +0200

    Update Simulink version checker

--- simulink/releaseManagement.m
+++ simulink/releaseManagement.m
@@ -1,7 +1,9 @@
-clc; clear;
+function releaseManagement()
 
-libNames={'AutomationShield','FloatLibrary','HeatLibrary','OptoLibrary','TugLibrary'};
-exNames={'OptoShield_InputsOutputs','OptoShield_PID_Control','OptoShield_Pulse_Identification','HeatShield_ID_Test','HeatShield_InputsOutputs','HeatShield_PID','HeatShield_PID_Simulate'};
+currVer='R2019a' %Currently supported Simulink version
+
+libNames={'AutomationShield','FloatLibrary','HeatLibrary','OptoLibrary','TugLibrary','MagnetoLibrary','PressureLibrary','MotoLibrary'};
+exampleList={'OptoShield','HeatShield','FloatShield','TugShield','PressureShield','MotoShield','MagnetoShield'};
 versions=[];
 
 disp('')
@@ -15,22 +17,45 @@ for i=1:length(libNames)
     versions{i}=info.ReleaseName;
     disp([libNames{i},': ',versions{i}])    
 end
+versions=[];
 
 disp('')
 disp('=======================')
 disp('')
+
+
+
 
 disp('Example Model Versions:')
 disp('-----------------------')
-for i=1:length(exNames)
-    info = Simulink.MDLInfo(exNames{i});
-    versions{i}=info.ReleaseName;
-    disp([exNames{i},': ',versions{i}])    
+cd('examples')
+for i=1:length(exampleList);
+    cd(exampleList{i})
+    for i=1:length(listDirSLX())
+        dirContents=listDirSLX();
+        scr = dirContents(i).name; %Full file name with extension
+        %scr = dirContents(i).name(1:end-2); %Script name to launch
+        info = Simulink.MDLInfo(scr);
+        versions{i}=info.ReleaseName;
+        disp([scr,': ',versions{i}])  
+    end
+    cd .. % Switch to next directory
 end
+cd ..
 
 disp('')
 disp('=======================')
 disp('')
+
+
+if all(strcmp(versions(:),currVer))
+    disp(['Current Simulink API and example version: ',currVer])
+else
+    error(['Error! At least one library or example is other than ',currVer])
+end
+
+
+
 
 if all(strcmp(versions(:),versions{1}))
     disp('All library modules and examples are saved in the same Simulink version.')
@@ -38,4 +63,11 @@ else
     disp('Warning! Not all modules and examples are saved in the same Simulink version!')
 end
 
+end
 
+
+function out = listDirSLX
+    persistent dirContents
+    dirContents=dir('**/*.slx');
+    out = dirContents;
+end
