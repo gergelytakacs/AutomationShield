12fd70c7638fcb316171dfc1392e86a07a6536db
Cenculis <92367957+cenculis@users.noreply.github.com>
2022-04-24 18:17:52 +0200

    Update AeroShield_speedTest.m

--- matlab/examples/AeroShield/AeroShield_speedTest/AeroShield_speedTest.m
+++ matlab/examples/AeroShield/AeroShield_speedTest/AeroShield_speedTest.m
@@ -2,17 +2,17 @@
 clear all
 clc 
 
-NumOSamples = 500;                                                  % Desired number of samples
+numOSamples = 500;                                                  % Desired number of samples
 platform = 'Due';                                                   % 'Uno', 'Due', etc.
 
 A = arduino('COM6',platform,'Libraries','I2C');                     % Creates arduino object
 configurePin(A,'A3','AnalogInput');                                 % Reads analog value from potentiometer
 configurePin(A,'D5','DigitalOutput')
 
-AS5600 = device(A,'I2CAddress',0x36,'bitrate',400000,'bus',1);  % Initializes I2c communication
-test = zeros(100,1);                                                 % Pre-allocate memory for execution time measurements
+AS5600 = device(A,'I2CAddress',0x36,'bitrate',400000,'bus',1);      % Initializes I2c communication
+test = zeros(100,1);                                                % Pre-allocate memory for execution time measurements
 disp('Testing...')                                                  % Message
-for i = 1:NumOSamples                                               % Performs several measurements
+for i = 1:numOSamples                                               % Performs several measurements
     tic;                                                            % Starts stopwatch
     y = readVoltage(A,'A3');                                        % Reads voltage from the Hall sensor to variable y
     writePWMVoltage(A,'D5', 0.2);
@@ -25,7 +25,7 @@ end
 writePWMVoltage(A,'D5', 0.0);
 disp('Done.')                                                       % Message.
 
-t = 1:NumOSamples;                                                   % Vector for axis
+t = 1:numOSamples;                                                   % Vector for axis
 plot(t,test,'o')                                                     % Plot results
 grid on;                                                            % Grid
 ylabel('Total execution time [s]')                                  % Y axis label
